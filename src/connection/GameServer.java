48fa5fac17f3e16449dec85ecd0b6c8735ed65aa
Perciii <qs.sauvage@gmail.com>
2019-04-12 11:24:41 +0200

    beginning of server

--- src/connection/GameServer.java
+++ src/connection/GameServer.java
@@ -1,6 +1,9 @@
 package connection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Observable;
@@ -11,8 +14,23 @@ public class GameServer {
 	//TODO
 	
 	public void createServer() throws IOException {
-		int port = 00;//to be determined
-		ServerSocket ss = new ServerSocket(port);
+		int port = 5000;//to be determined
+		PrintWriter out;
+		BufferedReader in;
+		try(ServerSocket ss = new ServerSocket(port)){
+			try(Socket s = ss.accept()) {
+				in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				out = new PrintWriter(s.getOutputStream(), true);
+				int i = 0;
+				while(i<10) {
+					String str = in.readLine();
+					//TODO : conditions
+					i++;
+				}
+			} catch(IOException ioe) {
+				System.err.println("Erreur : "+ioe);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
