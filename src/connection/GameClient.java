07e428411340cabf28f8aaa06aecfb8aac14fef9
Perciii <qs.sauvage@gmail.com>
2019-04-12 10:02:26 +0200

    add two classes just 4 fun

---
+++ src/connection/GameClient.java
@@ -0,0 +1,11 @@
+package connection;
+
+public class GameClient {
+	public void createClient() {
+		//TODO
+	}
+	
+	public static void main(String[] args) {
+		//TODO
+	}
+}
---
+++ src/connection/GameServer.java
@@ -0,0 +1,21 @@
+package connection;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+public class GameServer {
+	//TODO
+	
+	public void createServer() throws IOException {
+		int port = 00;//to be determined
+		ServerSocket ss = new ServerSocket(port);
+	}
+	
+	public static void main(String[] args) {
+		//TODO
+	}
+}
