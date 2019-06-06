ccbfa9be8e9dff13c470263b4c9eb7f7b7c242a7
Perciii <qs.sauvage@gmail.com>
2019-06-06 19:04:33 +0200

    localhost + jars

--- src/main/java/connection/GameClient.java
+++ src/main/java/connection/GameClient.java
@@ -27,7 +27,7 @@ public class GameClient implements Runnable {
 	public static void main(String[] args) {
 
 		int portNumber = 2222;
-		String host = "172.20.10.4";
+		String host = "localhost";
 
 		try {
 			clientSocket = new Socket(host, portNumber);
